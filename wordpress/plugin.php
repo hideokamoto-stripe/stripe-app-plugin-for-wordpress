bd938d5e31c836f0d29b6ea88bcc7f23498c8261
Hidetaka Okamoto <hideokamoto@stripe.com>
2024-02-05 10:47:48 +0900

    wordpress: oauth handler

--- wordpress/plugin.php
+++ wordpress/plugin.php
@@ -16,6 +16,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Add Setting UI
+ */
 add_action( 'admin_menu', 'stripe_sample_admin_menu' );
 add_action( 'admin_init', 'stripe_sample_admin_init' );
 function stripe_sample_admin_init() {
@@ -83,4 +86,144 @@ function stripe_sample_settings_page() {
         </form>
     </div>
 <?
+}
+
+/**
+ * Add callback endpoint
+ */
+add_action( 'rest_api_init', function() {
+    register_rest_route(
+        'stripe-apps/v1',
+        '/callback',
+        [
+            'methods' => 'GET',
+            'callback' => 'stripe_sample_callback_handler'
+        ]
+    );
+    register_rest_route(
+        'stripe-apps/v1',
+        '/customers',
+        [
+            'methods' => 'GET',
+            'callback' => 'stripe_sample_list_customers',
+        ]
+    );
+} );
+
+/**
+ * List Stripe Customers
+ */
+function stripe_sample_list_customers( WP_REST_Request $request ) {
+    $options = get_option( 'stripe_sample_oauth' );
+    if ( ! isset( $options) || empty( $options ) ) {
+        $error_response = new WP_REST_Response([
+            'error' => 'Permission denined.'
+        ] );
+        $error_response->set_status( 400 );
+        return $error_response;
+    }
+    $settings = json_decode( $options, true );
+    $refresh_token = $settings[ 'refresh_token' ];
+
+    $refreshed_data = stripe_sample_update_oauth_token( 'refresh_token', $refresh_token );
+    if ( is_wp_error( $refreshed_data ) ) {
+        return new WP_REST_Response( $refreshed_data );
+        $error_response = new WP_REST_Response([
+            'error' => $refreshed_data->get_error_message,
+            'code' => $refreshed_data->get_error_code,
+        ] );
+        $error_response->set_status( 400 );
+        return $error_response;
+    }
+    
+    $access_token = $refreshed_data[ 'access_token' ];
+    $livemode = $refreshed_data[ 'livemode' ];
+    $base_url = 'https://api.stripe.com/v1/'; 
+    $response = wp_remote_get( $base_url . 'customers', [
+        'headers' => [
+            'Authorization' => 'BASIC ' . base64_encode( $access_token ),
+        ]
+    ] );
+    $response_body = wp_remote_retrieve_body( $response );
+
+    return new WP_REST_Response( json_decode( $response_body, true )[ 'data' ] );
+}
+
+/**
+ * Get or refresh Stripe API access token
+ * 
+ * @param $grant_type 'authorization_code' | 'refresh_token': Grant type
+ * @param $code_or_token string: Authorization code of refresh token
+ */
+function stripe_sample_update_oauth_token( $grant_type, $code_or_token ) {
+    $stripe_secret_key = get_option( 'secret_api_key' );
+    if ( ! isset( $stripe_secret_key ) || empty( $stripe_secret_key ) ) {
+        return new WP_Error( 'stripe_api_key_required', 'You need to save Stripe API key at first' );
+    }
+    $auth_headers = [
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'Authorization' => 'BASIC ' . base64_encode( $stripe_secret_key ),
+    ];
+    $auth_body = [
+        'grant_type' => $grant_type,
+    ];
+    switch ( $grant_type ) {
+        case 'authorization_code':
+            $auth_body[ 'code' ] = $code_or_token;
+            break;
+        case 'refresh_token':
+            $auth_body[ 'refresh_token' ] = $code_or_token;
+            break;
+        default:
+            return new WP_Error( 'stripe_invalid_grant_type', $grant_type . ' is unsupported.' );
+    }
+    $auth_result = wp_remote_post( 'https://api.stripe.com/v1/oauth/token', [
+        'headers' => $auth_headers,
+        'body' => $auth_body,
+    ]);
+
+    if ( is_wp_error( $auth_result ) ) {
+        return $auth_result;
+        $error_message = $auth_result->get_error_message();
+        return new WP_Error( 'stripe_api_error', $error_message );
+    }
+    $auth_result_body = wp_remote_retrieve_body( $auth_result );
+    update_option( 'stripe_sample_oauth', $auth_result_body );
+    return json_decode( $auth_result_body, true );
+}
+
+/**
+ * OAuth callback handler
+ */
+function stripe_sample_callback_handler( WP_REST_Request $request ) {
+    return new WP_REST_Response( [
+        'api_key' => get_option( 'secret_api_key' ),
+        'stripe_sample_oauth_code' => get_option( 'stripe_sample_oauth_code' ),
+        'stripe_sample_oauth' => get_option( 'stripe_sample_oauth' )
+    ] );
+
+
+    $params = $request->get_query_params();
+    if ( ! isset( $params[ 'code' ] ) || empty( $params[ 'code' ] ) ) {
+        $error_response = new WP_REST_Response([
+            'error' => 'Missing required parameter'
+        ] );
+        $error_response->set_status( 400 );
+        return $error_response;
+    }
+    $code = $params[ 'code' ];
+    update_option( 'stripe_sample_oauth_code', $code );
+    $auth_result = stripe_sample_update_oauth_token( 'authorization_code', $code );
+    if ( is_wp_error( $auth_result ) ) {
+        $error_response = new WP_REST_Response([
+            'error' => $auth_result->get_error_message,
+            'code' => $auth_result->get_error_code,
+        ] );
+        $error_response->set_status( 400 );
+        return $error_response;
+    }
+
+    $response = new WP_REST_Response( $auth_result );
+    $response->set_status( 200 );
+    return $response;
 }
